97fbc18bd4baee618fc1f84a31d73f82857ebd7d
Adrian Moldovan <ma.moldovan@gmail.com>
2026-01-15 21:32:26 +0200

    Adds workshop 3 - fetch & bootstrap

---
+++ recap-workshop-03-fetch/main.js
@@ -0,0 +1,55 @@
+console.log('Rick & Morty API Example');
+
+const caracterCardsContainer = document.querySelector('#characters-container');
+
+
+fetch('https://rickandmortyapi.com/api/character?page=3')
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(character => {
+            const characterCard = document.createElement('div');
+            characterCard.classList.add('card','m-2');
+            characterCard.style.width = '18rem';
+            caracterCardsContainer.append(characterCard);
+
+            const cardImg = document.createElement('img');
+            cardImg.src = character.image;
+            cardImg.alt = `Picture of ${character.name}`;
+            cardImg.classList.add('card-img-top');
+            characterCard.append(cardImg);
+
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+            characterCard.append(cardBody);
+
+            const cardTitle = document.createElement('h2');
+            cardTitle.textContent = character.name;
+            cardBody.append(cardTitle);
+
+            const statusSpan = document.createElement('span');
+            const statusSpanClasses = ['badge'];
+            switch (character.status) {
+                case 'Alive':
+                    statusSpanClasses.push('text-bg-success');
+                    break;
+
+                case 'Dead':
+                    statusSpanClasses.push('text-bg-danger');
+                    break;
+            
+                default:
+                    statusSpanClasses.push('text-bg-secondary');    
+                    break;
+            }
+            statusSpan.classList.add(...statusSpanClasses);
+            statusSpan.textContent = character.status;
+            cardBody.append(statusSpan);
+
+            const detailsBtn = document.createElement('a')
+            detailsBtn.classList.add('btn', 'btn-primary', 'd-block', 'mt-3');
+            detailsBtn.textContent = 'Details';
+            detailsBtn.href = '#';
+            cardBody.append(detailsBtn);
+            
+        });
+    });
