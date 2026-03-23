bb9e4125fd395fcf91ee741f3aa04f143094d46b
Paul Negoescu <paul.negoescu@gmail.com>
2026-03-23 19:41:02 +0200

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Nav } from "./components/Nav/Nav";
 import { Counter } from "./features/Counter/Counter";
 import { Todos } from "./features/Todos/Todos";
 import { Weather } from "./features/Weather/Weather";
 
 export function App() {
   return (
-    <>
-      <Todos />
-      <Weather />
-      <Counter diff={5} initialCounterValue={3} />
-      <Counter />
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<h1>Homepage</h1>} />
+        <Route path="todos" element={<Todos />} />
+        <Route path="weather" element={<Weather />} />
+        <Route path="counter" element={<Counter diff={5} initialCounterValue={3} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
